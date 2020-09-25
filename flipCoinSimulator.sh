cd7a402385f113b61b11a0303ec2cb5bf3dbd3cc
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-25 12:10:08 +0530

    Add: conditional statement to determine the winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Simulator Program";
+
+#Coin toss
+toss=$((RANDOM%2));
+
+if [ $toss -eq 0 ];
+then
+	echo "Heads is the winner!";
+else
+	echo "Tails is the winner!";
+fi
